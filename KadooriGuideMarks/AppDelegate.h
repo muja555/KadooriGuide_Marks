9489f5688687358fb4b28d83556b05ef38974d19
Moja <moja@mojas-mac.local>
2016-01-03 06:03:11 -0800

    Initial Commit

---
+++ KadooriGuideMarks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KadooriGuideMarks
+//
+//  Created by Moja on 1/3/16.
+//  Copyright © 2016 MicroFuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KadooriGuideMarks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KadooriGuideMarks
+//
+//  Created by Moja on 1/3/16.
+//  Copyright © 2016 MicroFuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
